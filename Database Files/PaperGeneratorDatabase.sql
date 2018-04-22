de5cbe7c20a3502a097d65da073e0265622f717d
Bhargav J Patel <patelbhargav082@gmail.com>
2018-04-22 13:04:05 +0530

    d

--- Database Files/PaperGeneratorDatabase.sql
+++ Database Files/PaperGeneratorDatabase.sql
@@ -1524,7 +1524,591 @@ ELSE IF @Type = 'Chapter'
 END
 GO
 
-------Testting
+----------- Date : 22/04/2018 / By Devanshi Pujara
+
+ALTER PROCEDURE [dbo].[usp_GetQuetionTypeMaster]
+@DisplayLength int,
+@DisplayStart int,
+@SortCol int,
+@SortDir nvarchar(10),
+@Search nvarchar(255) = NULL,
+@compid AS NVARCHAR(250)
+
+AS
+
+BEGIN
+
+	DECLARE @FirstRec int, @LastRec int
+	SET @FirstRec = @DisplayStart;
+	SET @LastRec = @DisplayStart + @DisplayLength;
+
+	WITH CTE_TblMasterAssembly AS
+	(
+		SELECT ROW_NUMBER() OVER (ORDER BY 
+								CASE WHEN (@SortCol = 0 and @SortDir = 'asc') then	
+								cMedium END ASC,
+								CASE WHEN (@SortCol = 0 and @SortDir = 'desc') then	
+								cMedium END DESC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'asc') then	
+								cStandardName END ASC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'desc') then	
+								cStandardName END DESC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'asc') then	
+								cSubjectName END ASC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'desc') then	
+								cSubjectName END DESC,
+								CASE WHEN (@SortCol = 3 and @SortDir = 'asc') then	
+								cDescription END ASC,
+								CASE WHEN (@SortCol = 3 and @SortDir = 'desc') then	
+								cDescription END DESC
+								) AS RowNum,
+								count(*) OVER() as TotalCount,
+								mm.cMedium,
+								sm.cStandardName,
+								subm.cSubjectName,
+								QTM.cDescription,
+								'<a href="MasterQuestionType.aspx?ID='+ convert(nvarchar(100),cQuestion_Type_ID) +'&E=1" title="Edit"><img src="assets/img/edit.png" />' as cedit,
+								'<a href="MasterQuestionType.aspx?ID='+ convert(nvarchar(100),cQuestion_Type_ID) +'&D=1" title="Delete"><img src="assets/img/delete.png" /></a>' as cdelete
+								--'' as editdelete,
+								--'' as cedit, '' as cdelete
+								FROM Tbl_Question_Type_Master(nolock) QTM   
+								INNER JOIN Tbl_SubjectMaster(nolock) subm on subm.cSubject_ID = QTM.cSubject_ID
+								INNER JOIN Tbl_StandardMaster(nolock) sm on sm.cStandard_ID = QTM.cStandard_ID
+								INNER JOIN Tbl_MidiumMaster(nolock) mm ON mm.cMedium_ID = QTM.cMedium_ID
+								WHERE QTM.cCompany_ID =@compid and (@Search IS NULL
+										OR cMedium LIKE	'%' + @Search + '%'
+										OR cStandardName LIKE	'%' + @Search + '%'
+										OR cSubjectName LIKE	'%' + @Search + '%'
+										OR cDescription LIKE	'%' + @Search + '%'
+									)
+							)
+	
+	SELECT * FROM CTE_TblMasterAssembly
+	WHERE RowNum > @FirstRec AND RowNum <= @LastRec
+
+END
+
+--SP_ListMasterZone '5','0','0','asc',''
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_GetTopicMaster]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_GetTopicMaster]
+@DisplayLength int,
+@DisplayStart int,
+@SortCol int,
+@SortDir nvarchar(10),
+@Search nvarchar(255) = NULL,
+@compid AS NVARCHAR(250)
+
+AS
+
+BEGIN
+
+	DECLARE @FirstRec int, @LastRec int
+	SET @FirstRec = @DisplayStart;
+	SET @LastRec = @DisplayStart + @DisplayLength;
+
+	WITH CTE_TblMasterAssembly AS
+	(
+		SELECT ROW_NUMBER() OVER (ORDER BY 
+								CASE WHEN (@SortCol = 0 and @SortDir = 'asc') then	
+								cMedium END ASC,
+								CASE WHEN (@SortCol = 0 and @SortDir = 'desc') then	
+								cMedium END DESC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'asc') then	
+								cStandardName END ASC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'desc') then	
+								cStandardName END DESC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'asc') then	
+								cSubjectName END ASC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'desc') then	
+								cSubjectName END DESC,
+								CASE WHEN (@SortCol = 3 and @SortDir = 'asc') then	
+								cChapterName END ASC,
+								CASE WHEN (@SortCol = 3 and @SortDir = 'desc') then	
+								cChapterName END DESC,
+								CASE WHEN (@SortCol = 4 and @SortDir = 'asc') then	
+								nTopic_Index END ASC,
+								CASE WHEN (@SortCol = 4 and @SortDir = 'desc') then	
+								nTopic_Index END DESC,
+								CASE WHEN (@SortCol = 5 and @SortDir = 'asc') then	
+								cTopicName END ASC,
+								CASE WHEN (@SortCol = 5 and @SortDir = 'desc') then	
+								cTopicName END DESC
+								) AS RowNum,
+								count(*) OVER() as TotalCount,
+								mm.cMedium,
+								sm.cStandardName,
+								subm.cSubjectName,
+								cm.cChapterName,
+								tm.nTopic_Index,
+								tm.cTopicName,
+								'<a href="MasterTopic.aspx?ID='+ convert(nvarchar(100),cTopic_ID) +'&E=1" title="Edit"><img src="assets/img/edit.png" />' as cedit,
+								'<a href="MasterTopic.aspx?ID='+ convert(nvarchar(100),cTopic_ID) +'&D=1" title="Delete"><img src="assets/img/delete.png" /></a>' as cdelete
+								--'' as editdelete,
+								--'' as cedit, '' as cdelete
+								FROM Tbl_TopicMaster(nolock) tm 
+								INNER JOIN Tbl_ChapterMaster(nolock) cm on cm.cChapter_ID = tm.cChapter_ID
+								INNER JOIN Tbl_SubjectMaster(nolock) subm on subm.cSubject_ID = tm.cSubject_ID
+								INNER JOIN Tbl_StandardMaster(nolock) sm on sm.cStandard_ID = tm.cStandard_ID
+								INNER JOIN Tbl_MidiumMaster(nolock) mm ON mm.cMedium_ID = tm.cMedium_ID
+								WHERE tm.cCompany_ID =@compid and (@Search IS NULL
+										OR cMedium LIKE	'%' + @Search + '%'
+										OR cStandardName LIKE	'%' + @Search + '%'
+										OR cSubjectName LIKE	'%' + @Search + '%'
+										OR cChapterName LIKE	'%' + @Search + '%'
+										OR cTopicName LIKE	'%' + @Search + '%'
+									)
+							)
+	
+	SELECT * FROM CTE_TblMasterAssembly
+	WHERE RowNum > @FirstRec AND RowNum <= @LastRec
+
+END
+
+--SP_ListMasterZone '5','0','0','asc',''
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_GetComboList]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+ALTER PROCEDURE [dbo].[usp_GetComboList]
+@Type as varchar(20),
+@Id  as varchar(100),
+@cCompany_ID as varchar(100)
+as
+BEGIN
+IF @Type = 'Medium'
+	BEGIN
+		select '00000000-0000-0000-0000-000000000000' as cMedium_ID,'Select' as cMedium union
+		select  cMedium_ID , cMedium from Tbl_MidiumMaster(nolock) where cCompany_ID =  @cCompany_ID
+	END
+ELSE IF @Type = 'Standard'
+	BEGIN
+		select '00000000-0000-0000-0000-000000000000' as cStandard_ID,'Select' as cStandardName union
+		select cStandard_ID , cStandardName from Tbl_StandardMaster(nolock) where cMedium_ID = @Id and cCompany_ID =  @cCompany_ID
+	END
+ELSE IF @Type = 'Subject'
+	BEGIN
+		select '00000000-0000-0000-0000-000000000000' as cSubject_ID,'Select' as cSubjectName union
+		select cSubject_ID, cSubjectName from Tbl_SubjectMaster(nolock) where cStandard_ID = @Id and cCompany_ID =  @cCompany_ID
+	END
+ELSE IF @Type = 'Chapter'
+	BEGIN
+		select cChapter_ID , cChapterName from Tbl_ChapterMaster(nolock) where cSubject_ID  = @Id and cCompany_ID =  @cCompany_ID
+	END
+END
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_GetChapterMaster]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_GetChapterMaster]
+@DisplayLength int,
+@DisplayStart int,
+@SortCol int,
+@SortDir nvarchar(10),
+@Search nvarchar(255) = NULL,
+@compid AS NVARCHAR(250)
+
+AS
+
+BEGIN
+
+	DECLARE @FirstRec int, @LastRec int
+	SET @FirstRec = @DisplayStart;
+	SET @LastRec = @DisplayStart + @DisplayLength;
+
+	WITH CTE_TblMasterAssembly AS
+	(
+		SELECT ROW_NUMBER() OVER (ORDER BY 
+								CASE WHEN (@SortCol = 0 and @SortDir = 'asc') then	
+								cMedium END ASC,
+								CASE WHEN (@SortCol = 0 and @SortDir = 'desc') then	
+								cMedium END DESC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'asc') then	
+								cStandardName END ASC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'desc') then	
+								cStandardName END DESC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'asc') then	
+								cSubjectName END ASC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'desc') then	
+								cSubjectName END DESC,
+								CASE WHEN (@SortCol = 3 and @SortDir = 'asc') then	
+								nChaper_Index END ASC,
+								CASE WHEN (@SortCol = 3 and @SortDir = 'desc') then	
+								nChaper_Index END DESC,
+								CASE WHEN (@SortCol = 4 and @SortDir = 'asc') then	
+								cChapterName END ASC,
+								CASE WHEN (@SortCol = 4 and @SortDir = 'desc') then	
+								cChapterName END DESC
+								) AS RowNum,
+								count(*) OVER() as TotalCount,
+								mm.cMedium,
+								sm.cStandardName,
+								subm.cSubjectName,nChaper_Index,cChapterName,
+								'<a href="MasterChapter.aspx?ID='+ convert(nvarchar(100),cChapter_ID) +'&E=1" title="Edit"><img src="assets/img/edit.png" />' as cedit,
+								'<a href="MasterChapter.aspx?ID='+ convert(nvarchar(100),cChapter_ID) +'&D=1" title="Delete"><img src="assets/img/delete.png" /></a>' as cdelete
+								--'' as editdelete,
+								--'' as cedit, '' as cdelete
+								FROM Tbl_ChapterMaster(nolock) cm
+								INNER JOIN Tbl_SubjectMaster(nolock) subm on subm.cSubject_ID = cm.cSubject_ID
+								INNER JOIN Tbl_StandardMaster(nolock) sm on sm.cStandard_ID = cm.cStandard_ID
+								INNER JOIN Tbl_MidiumMaster(nolock) mm ON mm.cMedium_ID = cm.cMedium_ID
+								WHERE cm.cCompany_ID =@compid and (@Search IS NULL
+										OR cMedium LIKE	'%' + @Search + '%'
+										OR cStandardName LIKE	'%' + @Search + '%'
+										OR cSubjectName LIKE	'%' + @Search + '%'
+										OR cChapterName LIKE	'%' + @Search + '%'
+										
+									  )
+							)
+	
+	SELECT * FROM CTE_TblMasterAssembly
+	WHERE RowNum > @FirstRec AND RowNum <= @LastRec
+
+END
+
+--SP_ListMasterZone '5','0','0','asc',''
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_GetSubjectMaster]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_GetSubjectMaster]
+@DisplayLength int,
+@DisplayStart int,
+@SortCol int,
+@SortDir nvarchar(10),
+@Search nvarchar(255) = NULL,
+@compid AS NVARCHAR(250)
+
+AS
+
+BEGIN
+
+	DECLARE @FirstRec int, @LastRec int
+	SET @FirstRec = @DisplayStart;
+	SET @LastRec = @DisplayStart + @DisplayLength;
+
+	WITH CTE_TblMasterAssembly AS
+	(
+		SELECT ROW_NUMBER() OVER (ORDER BY 
+								CASE WHEN (@SortCol = 0 and @SortDir = 'asc') then	
+								cMedium END ASC,
+								CASE WHEN (@SortCol = 0 and @SortDir = 'desc') then	
+								cMedium END DESC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'asc') then	
+								cStandardName END ASC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'desc') then	
+								cStandardName END DESC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'asc') then	
+								cSubjectName END ASC,
+								CASE WHEN (@SortCol = 2 and @SortDir = 'desc') then	
+								cSubjectName END DESC
+								) AS RowNum,
+								count(*) OVER() as TotalCount,
+								mm.cMedium,
+								sm.cStandardName,
+								subm.cSubjectName,
+								'<a href="MasterSubject.aspx?ID='+ convert(nvarchar(100),cSubject_ID) +'&E=1" title="Edit"><img src="assets/img/edit.png" />' as cedit,
+								'<a href="MasterSubject.aspx?ID='+ convert(nvarchar(100),cSubject_ID) +'&D=1" title="Delete"><img src="assets/img/delete.png" /></a>' as cdelete
+								--'' as editdelete,
+								--'' as cedit, '' as cdelete
+								FROM Tbl_SubjectMaster(nolock) subm
+								INNER JOIN Tbl_StandardMaster(nolock) sm on sm.cStandard_ID = subm.cStandard_ID
+								INNER JOIN Tbl_MidiumMaster(nolock) mm ON mm.cMedium_ID = subm.cMedium_ID
+								WHERE subm.cCompany_ID =@compid and (@Search IS NULL
+										OR cMedium LIKE	'%' + @Search + '%'
+										OR cStandardName LIKE	'%' + @Search + '%'
+										OR cSubjectName LIKE	'%' + @Search + '%'
+										
+									  )
+							)
+	
+	SELECT * FROM CTE_TblMasterAssembly
+	WHERE RowNum > @FirstRec AND RowNum <= @LastRec
+
+END
+
+--SP_ListMasterZone '5','0','0','asc',''
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_GetLevelMaster]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_GetLevelMaster]
+@DisplayLength int,
+@DisplayStart int,
+@SortCol int,
+@SortDir nvarchar(10),
+@Search nvarchar(255) = NULL,
+@compid AS NVARCHAR(250)
+
+AS
+
+BEGIN
+
+	DECLARE @FirstRec int, @LastRec int
+	SET @FirstRec = @DisplayStart;
+	SET @LastRec = @DisplayStart + @DisplayLength;
+
+	WITH CTE_TblMasterAssembly AS
+	(
+		SELECT ROW_NUMBER() OVER (ORDER BY 
+								CASE WHEN (@SortCol = 0 and @SortDir = 'asc') then	
+								cLevelName END ASC,
+								CASE WHEN (@SortCol = 0 and @SortDir = 'desc') then	
+								cLevelName END DESC
+								) AS RowNum,
+								count(*) OVER() as TotalCount,
+								cLevelName,
+								'<a href="MasterLevel.aspx?ID='+ convert(nvarchar(100),cLevel_ID) +'&E=1" title="Edit"><img src="assets/img/edit.png" />' as cedit,
+								'<a href="MasterLevel.aspx?ID='+ convert(nvarchar(100),cLevel_ID) +'&D=1" title="Delete"><img src="assets/img/delete.png" /></a>' as cdelete
+								--'' as editdelete,
+								--'' as cedit, '' as cdelete
+								FROM Tbl_LevelMaster(nolock) LM   
+								WHERE LM.cCompany_ID =@compid and (@Search IS NULL
+										OR cLevelName LIKE	'%' + @Search + '%'
+								
+									)
+							)
+	
+	SELECT * FROM CTE_TblMasterAssembly
+	WHERE RowNum > @FirstRec AND RowNum <= @LastRec
+
+END
+
+--SP_ListMasterZone '5','0','0','asc',''
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_GetStandardMaster]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_GetStandardMaster]
+@DisplayLength int,
+@DisplayStart int,
+@SortCol int,
+@SortDir nvarchar(10),
+@Search nvarchar(255) = NULL,
+@compid AS NVARCHAR(250)
+
+AS
+
+BEGIN
+
+	DECLARE @FirstRec int, @LastRec int
+	SET @FirstRec = @DisplayStart;
+	SET @LastRec = @DisplayStart + @DisplayLength;
+
+	WITH CTE_TblMasterAssembly AS
+	(
+		SELECT ROW_NUMBER() OVER (ORDER BY 
+								CASE WHEN (@SortCol = 0 and @SortDir = 'asc') then	
+								cMedium END ASC,
+								CASE WHEN (@SortCol = 0 and @SortDir = 'desc') then	
+								cMedium END DESC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'asc') then	
+								cStandardName END ASC,
+								CASE WHEN (@SortCol = 1 and @SortDir = 'desc') then	
+								cStandardName END DESC
+								) AS RowNum,
+								count(*) OVER() as TotalCount,
+								mm.cMedium,
+								sm.cStandardName,
+								'<a href="MasterStandard.aspx?ID='+ convert(nvarchar(100),sm.cStandard_ID) +'&E=1" title="Edit"><img src="assets/img/edit.png" />' as cedit,
+								'<a href="MasterStandard.aspx?ID='+ convert(nvarchar(100),sm.cStandard_ID) +'&D=1" title="Delete"><img src="assets/img/delete.png" /></a>' as cdelete
+								--'' as editdelete,
+								--'' as cedit, '' as cdelete
+								FROM Tbl_StandardMaster(nolock) sm
+								INNER JOIN Tbl_MidiumMaster(nolock) mm ON mm.cMedium_ID = sm.cMedium_ID
+								WHERE sm.cCompany_ID =@compid and (@Search IS NULL
+										OR cMedium LIKE	'%' + @Search + '%'
+										OR cStandardName LIKE	'%' + @Search + '%'
+									  )
+							)
+	
+	SELECT * FROM CTE_TblMasterAssembly
+	WHERE RowNum > @FirstRec AND RowNum <= @LastRec
+
+END
+
+--SP_ListMasterZone '5','0','0','asc',''
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_CurdStandardMaster]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_CurdStandardMaster]
+(
+@Type AS VARCHAR(10),
+@cStandard_ID AS VARCHAR(100),
+@cMedium_ID AS VARCHAR(100),
+@cStandardName AS NVARCHAR(100),
+@cCompany_ID AS VARCHAR(100),
+@cInsetedBy AS VARCHAR(100)
+)
+
+AS
+BEGIN
+SET NOCOUNT ON;
+SET DATEFORMAT DMY
+
+BEGIN TRANSACTION;
+Declare @ErrorMsg nvarchar(4000)
+
+SET DATEFORMAT DMY
+
+BEGIN TRY
+	if @Type='I'
+	begin
+		INSERT INTO dbo.Tbl_StandardMaster (cMedium_ID, cStandardName, cCompany_ID, cInsetedBy, dInserted_Date)
+		VALUES (@cMedium_ID, @cStandardName, @cCompany_ID, @cInsetedBy, getdate())
+	end
+
+	if @Type='U'
+	begin
+		UPDATE dbo.Tbl_StandardMaster
+		SET cMedium_ID = @cMedium_ID,
+			cStandardName = @cStandardName,
+			cUpdatedBy = @cInsetedBy,
+			dUpdated_Date = GETDATE()
+		WHERE cStandard_ID = @cStandard_ID 
+	end
+	if @Type='D'
+	begin
+		DELETE FROM Tbl_StandardMaster WHERE cStandard_ID = @cStandard_ID	
+	end
+
+COMMIT TRANSACTION;
+END TRY
+BEGIN CATCH
+	select @ErrorMsg = ERROR_MESSAGE();
+	if (XACT_STATE() = -1)
+		begin
+			ROLLBACK TRANSACTION;
+			RAISERROR(@ErrorMsg,16,1);
+		end
+	if (XACT_STATE() = 1)
+		begin
+			COMMIT TRANSACTION;
+			RAISERROR(@ErrorMsg,16,1);
+		end
+END CATCH
+END
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_SearchMaster]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_SearchMaster]
+@Type as varchar(20),
+@Id  as varchar(100)
+as
+begin
+
+if @type='Medium'
+begin
+	select cMedium from Tbl_MidiumMaster(nolock) where cMedium_ID=@id
+end
+ELSE if @type='Standard'
+begin
+	select cStandard_ID , cStandardName , cMedium_ID  from Tbl_StandardMaster(nolock) where cStandard_ID =@id
+end
+ELSE IF @type = 'Subject'
+ 	BEGIN
+ 		SELECT cMedium_ID,cStandard_ID,cSubjectName FROM Tbl_SubjectMaster(nolock) WHERE cSubject_ID = @id
+ 	END
+ELSE IF @type = 'Chapter'
+ 	BEGIN
+ 		SELECT  cMedium_ID,cStandard_ID,cSubject_ID,nChaper_Index,cChapterName,bDemo  FROM Tbl_ChapterMaster(nolock) WHERE cChapter_ID = @id
+ 	END
+ELSE IF @type = 'Topic'
+ 	BEGIN
+ 		SELECT cMedium_ID,cStandard_ID,cSubject_ID,cChapter_ID,nTopic_Index,cTopicName  FROM Tbl_TopicMaster(nolock) WHERE cTopic_ID = @id
+ 	END
+ELSE IF @type = 'QuestionTypeMaster'
+ 	BEGIN
+ 		SELECT cMedium_ID,cStandard_ID,cSubject_ID,cDescription  FROM Tbl_Question_Type_Master(nolock) WHERE cQuestion_Type_ID = @id
+ 	END
+ELSE IF @type = 'LevelMaster'
+ 	BEGIN
+ 		SELECT cLevelName  FROM Tbl_LevelMaster(nolock) WHERE cLevel_ID = @id
+ 	END
+END
+
+GO
+
+/****** Object:  StoredProcedure [dbo].[usp_GetLogin]    Script Date: 4/22/2018 12:15:27 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+ALTER PROCEDURE [dbo].[usp_GetLogin]
+@type as nvarchar(250),
+@username as nvarchar(250),
+@password as nvarchar(250)
+
+as
+begin
+
+		select l.cLogin_ID,cname,l.cCompany_ID ,cCompName
+		from login l,TblCompany c
+		where l.cCompany_ID=c.cCompany_ID and cUserName=@username and cPassword=@password 
+		and cactive='1'
+end
+
+GO
+
+
+
 
 
 
